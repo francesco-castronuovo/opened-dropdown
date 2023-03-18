8f8fa494f3a4f2112651bd2ead57fc4e23d14ce6
Francesco Castronuovo <118483640+francesco-castronuovo@users.noreply.github.com>
2023-03-18 18:36:24 +0100

    Update main.js
    Added the ability to open a dropdown based on a button clicked by the user in a different page.

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
   
-  const dropdown = document.querySelector('[fc-dropdown = opened]')
-  const dropdownToggle = dropdown.querySelector('.w-dropdown-toggle')
+  const identifier = sessionStorage['fc-dropdown-toggle']
   
-  dropdownToggle.dispatchEvent(new Event('mousedown'));
-  dropdownToggle.dispatchEvent(new Event('mouseup'));
-	
-});
+  if(identifier !== undefined)
+  {  
+  	const toggles = document.querySelectorAll('[fc-dropdown-toggle]')
+    let toggleIdentifiers = []
+    for(const toggle of toggles)
+      toggleIdentifiers.push(toggle.getAttribute('fc-dropdown-toggle'))
+
+  	console.log(toggleIdentifiers)
+  
+    for(let i = 0; i < toggleIdentifiers.length; i++)
+    {
+    	if(identifier === toggleIdentifiers[i])
+      {
+        const toggleToTrigger = toggles[i]
+        toggleToTrigger.dispatchEvent(new Event('mousedown'))
+        toggleToTrigger.dispatchEvent(new Event('mouseup'))
+      }
+    }
+  }
+  else
+  {
+  	const dropdown = document.querySelector('[fc-dropdown = default]')
+    const dropdownToggle = dropdown.querySelector('.w-dropdown-toggle')
+
+    dropdownToggle.dispatchEvent(new Event('mousedown'));
+    dropdownToggle.dispatchEvent(new Event('mouseup'));
+  }
+})
