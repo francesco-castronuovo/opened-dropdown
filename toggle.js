d0a06d23aa2bb99e00f0d4558ed110d2508672a3
Francesco Castronuovo <118483640+francesco-castronuovo@users.noreply.github.com>
2023-03-18 18:38:13 +0100

    Create toggle.js
    Allows the user to click on a link to open a specific dropdown inside another page.

---
+++ toggle.js
@@ -0,0 +1,23 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  const toggles = document.querySelectorAll('[fc-dropdown-toggle]')
+  sessionStorage['fc-dropdown-toggle'] = undefined
+
+  for(const toggle of toggles)
+  {
+    toggle.addEventListener('click', function(event)
+    {
+      const identifier = toggle.getAttribute('fc-dropdown-toggle')
+
+      if(sessionStorage['fc-dropdown-toggle'] === identifier)
+      {
+        console.log('true')
+        return;
+      }
+
+      event.preventDefault()
+      sessionStorage['fc-dropdown-toggle'] = identifier
+      toggle.click()
+    })
+  }
+})
